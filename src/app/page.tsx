d125341b874c7827bfbc298146771801b60af230
huynhthiphuoc11 <huynhthiphuoc04@gmail.com>
2025-09-07 11:39:47 +0700

    09: projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,27 +6,34 @@ import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 import { toast } from "sonner";
 import { Input } from "@/components/ui/input";
-import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+
 const Page =  () => {
+  const router = useRouter();
   const [value, setvalue] = useState("");
+
   const trpc = useTRPC();
-  const { data: message } = useQuery(trpc.messages.getMany.queryOptions())
-  const createMessage = useMutation(trpc.messages.create.mutationOptions({
-    onSuccess: () => {
-      toast.success("Message created");
+  const createProject = useMutation(trpc.projects.create.mutationOptions({
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      router.push(`/projects/${data.id}`);
     }
   }));
   return(
-    <div className= "p-4 max-w-7xl mx-auto">
-      <Input value = {value} onChange={(e) => setvalue(e.target.value)}/>
-      <Button 
-      disabled={createMessage.isPending} 
-      onClick={() => createMessage.mutate({ value: value })}>
-        Create Message
-      </Button>
-      {JSON.stringify(message, null, 2)}
+    <div className= "h-screen w-screen flex items-center justify-center">
+      <div className= "max-w-7xl mx-auto flex items-center flex-col gap-y-4 justify-center"> 
+        <Input value = {value} onChange={(e) => setvalue(e.target.value)}/>
+        <Button 
+          disabled={createProject.isPending} 
+          onClick={() => createProject.mutate({ value: value })}>
+          Submit
+        </Button>
+      </div>
     </div>
   );
-  
 };
 export default Page;
--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -17,12 +17,16 @@ export const messageRouter = createTRPCRouter({
     create: baseProcedure
       .input(
         z.object({
-            value: z.string().min(1, { message: "Message is required"}),
+            value: z.string()
+            .min(1, { message: "Message is required"})
+            .max(1000, { message: "Message is too long"}),
+            projectId: z.string().min(1, { message: "Project ID is required"}),
         }),
       )
       .mutation(async ({ input, ctx }) => {
         const createMessage =await prisma.message.create({
             data: {
+                projectId: input.projectId,
                 content: input.value,
                 role: "USER",
                 type: "RESULT",
@@ -32,6 +36,7 @@ export const messageRouter = createTRPCRouter({
             name: "code-agent/run",
             data: {
                 value: input.value,
+                projectId: input.projectId,
             },
         });
         return createMessage;
